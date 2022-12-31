9958585674ab35f478ba3ea729e7197bb37f39e7
ARKA PRAVO DUTTA <77730711+arkafreak@users.noreply.github.com>
2022-12-31 18:11:27 +0530

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import sklearn
 import pandas as pd
 import matplotlib.pyplot as plt
-import sklearn
+
 
 def shorten_categories(categories, cutoff):
     categorical_map = {}
